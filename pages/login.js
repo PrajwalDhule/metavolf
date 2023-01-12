1fd27027c6168134901b1dc44eb18c3aef5dfb1a
Prajwal <prajwaldhule36@gmail.com>
2023-01-12 10:28:10 +0530

    Initial Commit

---
+++ pages/login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+// import "../styles/Login.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("email:", email);
+    console.log("password:", password);
+  }
+
+  return (
+    <div className="login-body body">
+      <div className="login-page-container">
+        <form onSubmit={handleSubmit}>
+          <label for="email">Email address:</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <label for="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
