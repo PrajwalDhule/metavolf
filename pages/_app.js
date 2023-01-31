101af92dc9f5af69f16ffd02127da62a66869da4
Prajwal <prajwaldhule36@gmail.com>
2023-02-01 00:59:56 +0530

    detached all css files

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
-import "../styles/Login.css";
-import "../styles/Navbar.css";
-import "../styles/Hero.css";
-import "../styles/HomeServices.css";
-import "../styles/WhyUs.css";
-import "../styles/testimonial.css";
-import "../styles/mediaQueries.css";
-import "../styles/Footer.css";
+// import "../styles/Login.css";
+// import "../styles/Navbar.css";
+// import "../styles/Hero.css";
+// import "../styles/HomeServices.css";
+// import "../styles/WhyUs.css";
+// import "../styles/testimonial.css";
+// import "../styles/mediaQueries.css";
+// import "../styles/Footer.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
