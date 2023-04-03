9719d1b90d32d1e6b5e63d5204635b7ac63ca994
Prajwal <prajwaldhule36@gmail.com>
2023-04-03 21:26:17 +0530

    progress bar done

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -8,6 +8,7 @@ const Dashboard = () => {
   const [singleServiceData, setSingleServiceData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [userEmail, setUserEmail] = useState(null);
+  const [percent, setPercent] = useState(60);
   // const [user, loadinguser, error] = useAuthState(auth);
 
   useEffect(() => {
@@ -149,16 +150,59 @@ const Dashboard = () => {
                 : serviceData[0].serviceDescription}
             </p>
           </div>
-          <div>
-            <p className="text-green-500">active</p>
+          <div className="flex flex-col items-center">
+            <p className="text-green-500">
+              {singleServiceData
+                ? singleServiceData.serviceStatus
+                : serviceData[0].serviceStatus}
+            </p>
             <div className="mt-4">
-              Progress:{" "}
-              <p className="text-3xl">
-                {singleServiceData
-                  ? singleServiceData.serviceProgress
-                  : serviceData[0].serviceProgress}{" "}
-                %
-              </p>
+              <p className="mb-4">Progress:</p>
+              <div className="text-3xl">
+                <div className="fixed inline-flex items-center justify-center overflow-hidden rounded-full scale-150">
+                  <svg className="w-20 h-20 ">
+                    <circle
+                      className="text-gray-300"
+                      strokeWidth="5"
+                      stroke="currentColor"
+                      fill="transparent"
+                      r="30"
+                      cx="40"
+                      cy="40"
+                    />
+                    <circle
+                      className="text-green-700"
+                      strokeWidth="5"
+                      strokeDasharray="188.5"
+                      strokeDashoffset={
+                        188.5 -
+                        ((singleServiceData
+                          ? singleServiceData.serviceProgress
+                          : serviceData[0].serviceProgress) /
+                          100) *
+                          188.5
+                      }
+                      strokeLinecap="round"
+                      stroke="currentColor"
+                      fill="transparent"
+                      r="30"
+                      cx="40"
+                      cy="40"
+                    />
+                  </svg>
+                  <span className="absolute text-sm text-white font-medium">
+                    {`${
+                      (singleServiceData
+                        ? Number(singleServiceData.serviceProgress)
+                        : Number(serviceData[0].serviceProgress)) !== NaN &&
+                      (singleServiceData
+                        ? Number(singleServiceData.serviceProgress)
+                        : Number(serviceData[0].serviceProgress)
+                      ).toFixed()
+                    }%`}
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </section>
@@ -175,7 +219,35 @@ const Dashboard = () => {
           <div>
             {/* <p className="text-green-500">active</p> */}
             <div className="mt-4">
-              Progress: <p className="text-3xl">60%</p>
+              <p className="mb-4">Progress:</p>
+              <div className="text-3xl">
+                <div className="fixed inline-flex items-center justify-center overflow-hidden rounded-full scale-150">
+                  <svg className="w-20 h-20 ">
+                    <circle
+                      className="text-gray-300"
+                      strokeWidth="5"
+                      stroke="currentColor"
+                      fill="transparent"
+                      r="30"
+                      cx="40"
+                      cy="40"
+                    />
+                    <circle
+                      className="text-green-700"
+                      strokeWidth="5"
+                      strokeDasharray="188.5"
+                      strokeDashoffset={188.5 - (percent / 100) * 188.5}
+                      strokeLinecap="round"
+                      stroke="currentColor"
+                      fill="transparent"
+                      r="30"
+                      cx="40"
+                      cy="40"
+                    />
+                  </svg>
+                  <span className="absolute text-sm text-white font-medium">{`${percent.toFixed()}%`}</span>
+                </div>
+              </div>
             </div>
           </div>
         </section>
